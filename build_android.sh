63eb6a088a5d1c3fd01bf98fea6b4e39836a8dfe
Alexander Lochmann <alexander.lochmann@tu-dortmund.de>
2013-09-17 22:36:43 +0200

    Fixed android build script: Only toolchains build against linux/libc are allowed

--- build_android.sh
+++ build_android.sh
@@ -5,7 +5,7 @@ FLAGS="-static -DANDROID"
 OUTPUT_DIR="android_binaries"
 STAPIO_BIN="staprun/stapio"
 STAPRUN_BIN="staprun/staprun"
-COMPILER_PREFIX_LIST=("arm-eabi" "arm-none-linux-gnueabi" "arm-none-eabi")
+COMPILER_PREFIX_LIST=("arm-none-linux-gnueabi")
 
 #export PATH="/usr/local/CodeSourcery/Sourcery_G++_Lite/bin:$PATH"
 
@@ -31,7 +31,7 @@ if [ -e "Makefile" ]; then
 	make clean
 fi
 
-ac_cv_prog_have_jar=no ac_cv_prog_have_javac=no ac_cv_with_java=no ac_cv_file__usr_include_avahi_common=no ac_cv_file__usr_include_avahi_client=no ac_cv_file__usr_include_nspr=no ac_cv_file__usr_include_nspr4=no ac_cv_file__usr_include_nss=no ac_cv_file__usr_include_nss3=no ac_cv_func_malloc_0_nonnull=yes ./configure --prefix=$BUILD_PREFIX --host=$COMPILER_PREFIX CXXFLAGS="$FLAGS" CFLAGS="$FLAGS" --disable-translator --disable-docs --disable-refdocs  --disable-grapher --without-rpm --without-nss
+ac_cv_with_java=no ac_cv_prog_have_jar=no ac_cv_prog_have_javac=no ac_cv_with_java=no ac_cv_file__usr_include_avahi_common=no ac_cv_file__usr_include_avahi_client=no ac_cv_file__usr_include_nspr=no ac_cv_file__usr_include_nspr4=no ac_cv_file__usr_include_nss=no ac_cv_file__usr_include_nss3=no ac_cv_func_malloc_0_nonnull=yes ./configure --prefix=$BUILD_PREFIX --host=$COMPILER_PREFIX CXXFLAGS="$FLAGS" CFLAGS="$FLAGS" --disable-translator --disable-docs --disable-refdocs  --disable-grapher --without-rpm --without-nss
 
 if [ $? -ne 0 ]; then
 	echo "Error configuring the workspace"
