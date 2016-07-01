c4b6120dee1eb3560c08898d09f713c0390f6439
Alexander Lochmann <alexander.lochmann@tu-dortmund.de>
2016-07-01 15:40:29 +0200

    Linux kernel 3.0.x was obviously a long-term kernel. Otherwise, a sublevel above 100 would not have been possible. Changed version check to cover 3.0.0 until 3.0.200. That should be sufficient.

--- runtime/linux/autoconf-asm-syscall.c
+++ runtime/linux/autoconf-asm-syscall.c
@@ -1,5 +1,5 @@
 #include <linux/version.h>
-#if LINUX_VERSION_CODE <= KERNEL_VERSION(3,0,100) && LINUX_VERSION_CODE >= KERNEL_VERSION(3,0,0)
+#if LINUX_VERSION_CODE <= KERNEL_VERSION(3,0,200) && LINUX_VERSION_CODE >= KERNEL_VERSION(3,0,0)
 #include <asm/unistd.h>
 #include <linux/sched.h>
 #include <asm-generic/syscall.h>
--- runtime/syscall.h
+++ runtime/syscall.h
@@ -110,7 +110,7 @@
 
 /* If the system has asm/syscall.h, use defines from it. */
 #include <linux/version.h>
-#if LINUX_VERSION_CODE <= KERNEL_VERSION(3,0,100) && LINUX_VERSION_CODE >= KERNEL_VERSION(3,0,0)
+#if LINUX_VERSION_CODE <= KERNEL_VERSION(3,0,200) && LINUX_VERSION_CODE >= KERNEL_VERSION(3,0,0)
 #include <asm/unistd.h>
 #include <linux/sched.h>
 #include <asm-generic/syscall.h>
